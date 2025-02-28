d01071a1bd3d6e44cdc3cfda30305189b3465bd3
hijaziimarwa <hijaziimarwa@gmail.com>
2025-02-28 22:50:49 +0200

    Getter method for rent price since it is private attribute

--- car_rental_service.py
+++ car_rental_service.py
@@ -5,6 +5,9 @@ class RentalVehicle():
         self.year = year
         self.__rental_price_per_day = rental_price_per_day
 
+    def get_rental_price_per_day(self):
+        return self.__rental_price_per_day
+
     def display_vehicle_info(self):
         print(f"Brand : {self.brand}, Model : {self.model} , Year : {self.year}"
               f" , Rental price : ${self.__rental_price_per_day}/day")    
