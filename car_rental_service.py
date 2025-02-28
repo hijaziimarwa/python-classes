f46a163ebd97b3cc3c23e1416cb1230443d34333
hijaziimarwa <hijaziimarwa@gmail.com>
2025-02-28 22:52:55 +0200

    Calculate the rental cost for a given number of days

--- car_rental_service.py
+++ car_rental_service.py
@@ -10,7 +10,10 @@ class RentalVehicle():
 
     def set_rental_price_per_day(self,rental_price_per_day):
         self.__rental_price_per_day = rental_price_per_day
-        
+
+    def calculate_rental_cost(self,days):
+        return self.__rental_price_per_day * days
+    
     def display_vehicle_info(self):
         print(f"Brand : {self.brand}, Model : {self.model} , Year : {self.year}"
               f" , Rental price : ${self.__rental_price_per_day}/day")    
