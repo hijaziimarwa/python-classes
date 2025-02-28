cf40d6d6831027848fd73a8ac79551705525ad1f
hijaziimarwa <hijaziimarwa@gmail.com>
2025-02-28 22:49:07 +0200

    Display vehicle info function

--- car_rental_service.py
+++ car_rental_service.py
@@ -4,3 +4,7 @@ class RentalVehicle():
         self.model = model
         self.year = year
         self.__rental_price_per_day = rental_price_per_day
+
+    def display_vehicle_info(self):
+        print(f"Brand : {self.brand}, Model : {self.model} , Year : {self.year}"
+              f" , Rental price : ${self.__rental_price_per_day}/day")    
